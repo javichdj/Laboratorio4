75d5ebff44c881b8f58db44164dfaa1c35225fff
JazminVS <jazminv8.95@gmail.com>
2016-05-12 08:50:34 -0500

    Mi version final

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,9 +1,14 @@
 #frase=input("INGRESE UNA FRASE POR FAVOR: ")
 #print("Numero de palabras: " + str(len(frase.split(" "))))
-f=open("frasepr.txt","w")
-f.write('Esto es una prueba\n')
-f.close()
-f=open("frasepr.txt")
-f.read()
-s=f.read()
-print("Numero de palabras: " + str(len(s.split(" "))))
+def openread():
+	f=open("frasepr.txt","w")
+	f.write('Esto es una prueba\n')
+	f.close()
+	
+def leer ():
+	f=open("frasepr.txt")
+	linea=f.readline()
+	print("Numero de palabras: " + str(len(linea.split(" "))))
+
+openread()
+leer()
