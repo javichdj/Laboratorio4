228ce6fa9b7b9269123a4d37965cef87a7488439
JazminVS <jazminv8.95@gmail.com>
2016-05-12 07:40:02 -0500

    Revert "Revert "Este es una prueba (prueba 1)""
    This reverts commit 055022f49100a3ff37c3de756e64ac3a3f67c48c.

---
+++ ejercicio6.py
@@ -0,0 +1,18 @@
+a=float(input("INGRESE SU ESTATURA(en m): "))
+b=float(input("INGRESE SU PESO(en kg): "))
+
+def indice_mm ():
+	indice=b/(a*a)
+	return indice
+
+resp=indice_mm()
+print("Su indice de masa corporal es: "+str(round(resp,2)))
+
+if(resp<18):		
+	print ("SU PESO ESTA POR DEBAJO DEL PROMEDIO")
+if(resp>=18 and resp<=24.9):
+	print ("SU PESO ES NORMAL")
+if(resp>=25 and resp<=26.9):
+	print ("UD TIENE SOBREPESO")
+if(resp>=27):
+	print ("UD TIENE OBESIDAD")
