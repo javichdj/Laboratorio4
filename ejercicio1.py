a46475f39f6a1ff12dd528c7fb7a197400791130
Javier <javich_dj@hotmail.com>
2016-05-12 08:59:22 -0500

    quite funcion
    se quita la funcion de crear un archivo, solo se deja que se llame un
    archivo

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,14 +1,7 @@
-#frase=input("INGRESE UNA FRASE POR FAVOR: ")
-#print("Numero de palabras: " + str(len(frase.split(" "))))
-def openwrite():
-	f=open("frasepr.txt","w")
-	f.write('Esto es una prueba\n')
-	f.close()
-	
 def read ():
 	f=open("frasepr.txt")
 	linea=f.readline()
 	print("Numero de palabras: " + str(len(linea.split(" "))))
 
-openwrite()
+
 read()
